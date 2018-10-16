584582ed08c0153e0be91df78d3c185f2ad5b708
Ernesto Iser <ernestoj.ic@gmail.com>
2018-10-16 11:25:45 -0700

    add space age exercise

---
+++ space-age/space_age.go
@@ -0,0 +1,11 @@
+package space
+
+// Planet struct
+type Planet struct {
+	planet string
+}
+
+// Age returns the age of a person on a given planet
+func Age(sec float64, planet Planet) float64 {
+	return 1
+}
